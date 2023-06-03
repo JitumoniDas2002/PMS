10f0f27d24ce25005e1c939d22b5b3d3a6e32ad3
NoisyBotDude <ashishsarmah11@gmail.com>
2023-06-03 23:23:30 +0530

    feat: added new contents

--- src/components/Navbaar.js
+++ src/components/Navbaar.js
@@ -1,4 +1,9 @@
 import React from "react";
+import "../App.css";
+import Dropdown from "react-bootstrap/Dropdown";
+import LoginIcon from "../images/Enter.png";
+import whiteProfile from "../images/profile-white.png";
+import logoutIcon from "../images/Out.png";
 
 export default function Navbaar() {
 
@@ -12,43 +17,55 @@ export default function Navbaar() {
 
     return (
         <div>
-            <nav className="navbar navbar-expand-lg bg-body-tertiary mb-5">
-                <div className="container-fluid">
-                    <a className="navbar-brand" href="/">PMS</a>
-                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"></span>
-                    </button>
-                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                            <li className="nav-item">
-                                <a className="nav-link active" aria-current="page" href="/">Home</a>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link" href="#">Link</a>
-                            </li>
-                            <li className="nav-item dropdown">
-                                <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                    Dropdown
-                                </a>
-                                <ul className="dropdown-menu">
-                                    <li><a className="dropdown-item" href="/login">Login</a></li>
-                                    <li><a className="dropdown-item" href="/signup">Sign Up</a></li>
-                                    <li><a className="dropdown-item">
-                                        <button onClick={handleLogOut} className="bg-transparent border-0">
-                                            Logout
-                                        </button>
-                                    </a></li>
-                                    <li><hr className="dropdown-divider" /></li>
-                                    <li><a className="dropdown-item" href="#">Something else here</a></li>
-                                </ul>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link disabled">Disabled</a>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-            </nav>
+            <nav className="navbaar">
+          <div
+            className="d-flex flex-row justify-content-between align-items-center w-100"
+          >
+            <div className="col-1 ">
+              <a href="/home">
+                PMS
+              </a>
+            </div>
+            <ul
+              className="d-flex flex-row col-3 list-unstyled justify-content-between align-items-center m-0"
+            >
+              <li className="col-sm-4 small">
+                <a href="/search" className="text-decoration-none text-dark">
+                  Search
+                </a>
+              </li>
+              <li className="col-sm-4 small">
+                <a href="/folder" className="text-decoration-none text-dark">
+                  Folders
+                </a>
+              </li>
+              <li className="col-sm-4 small">
+                <a href="/history" className="text-decoration-none text-dark">
+                  History
+                </a>
+              </li>
+            </ul>
+            <ul
+              className="d-flex flex-row col-3 list-unstyled justify-content-between align-items-center m-0"
+            >
+              <li className="col-sm-4 small">
+                <a href="/login" className="text-decoration-none text-dark">
+                  Login
+                </a>
+              </li>
+              <li className="col-sm-4 small">
+                <a href="/signup" className="text-decoration-none text-dark">
+                  Sign Up
+                </a>
+              </li>
+              <li className="col-sm-4 small">
+                <a href="/logout" className="text-decoration-none text-dark">
+                  Log Out
+                </a>
+              </li>
+            </ul>
+          </div>
+        </nav>
         </div>
     )
 }
